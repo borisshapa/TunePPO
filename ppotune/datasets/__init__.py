8c875b977f1d5ac2191a02e3b63c5e43119767e6
Arkadiy Vladimirov <ark.vladimirov@outlook.com>
2025-04-17 00:18:30 +0300

    config aligned with current structure

--- ppotune/datasets/__init__.py
+++ ppotune/datasets/__init__.py
@@ -1,9 +1,11 @@
 from ppotune.datasets.text_completion_dataset import (
-    text_completion_dataset,
-    TextCompletionDataset,
+    text_completion_dataset
+)
+from ppotune.datasets.gsm8k import (
+    gsm8k_dataset,
 )
 
 __all__ = [
-    "text_completion_dataset",
-    "TextCompletionDataset",
+    "text_completion_dataset"
+    "gsm8k_dataset"
 ]
