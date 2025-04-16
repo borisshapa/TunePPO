fedf129cdb5b15b46eda21a374a9b1e23e8b8a8e
Evgurov <evgurovv@yandex.ru>
2025-04-17 01:49:07 +0300

    alpaca dataset done

--- ppotune/datasets/__init__.py
+++ ppotune/datasets/__init__.py
@@ -2,8 +2,14 @@ from ppotune.datasets.text_completion_dataset import (
     text_completion_dataset,
     TextCompletionDataset,
 )
+from ppotune.datasets.alpaca_dataset import (
+    alpaca_dataset,
+    AlpacaDataset,
+)
 
 __all__ = [
     "text_completion_dataset",
     "TextCompletionDataset",
+    "alpaca_dataset",
+    "AlpacaDataset"
 ]
