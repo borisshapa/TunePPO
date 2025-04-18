326c9bfd2cf29182cb623660572d2b58b10fca54
Evgurov <evgurovv@yandex.ru>
2025-04-18 23:47:31 +0300

    arbiters added

---
+++ ppotune/arbiters/__init__.py
@@ -0,0 +1,11 @@
+from ppotune.arbiters.default_arbiter import (
+    default_arbiter
+)
+from ppotune.arbiters.tldr_arbiter import (
+    tldr_arbiter
+)
+
+__all__ = [
+    "default_arbiter",
+    "tldr_arbiter",
+]
