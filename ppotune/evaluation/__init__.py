933ebfeacfc8f9bb43e99cd4696d109e3377edec
Evgurov <evgurovv@yandex.ru>
2025-04-24 04:17:29 +0300

    gsm8k evaluator added

--- ppotune/evaluation/__init__.py
+++ ppotune/evaluation/__init__.py
@@ -2,8 +2,12 @@ from ppotune.evaluation.eval import (
     Evaluator,
     reference_completion_evaluator,
 )
+from ppotune.evaluation.gsm8k_eval import (
+    gsm8k_evaluator
+)
 
 __all__ = [
     "Evaluator",
-    "reference_completion_evaluator"
+    "reference_completion_evaluator",
+    "gsm8k_evaluator"
 ]
