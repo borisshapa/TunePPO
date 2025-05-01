quick fix for kl logging

ppotune/loss.py
@@ -35,7 +35,7 @@ class KLPenalty(nn.Module):
         per_token_kl = torch.exp(rhs_logprobs - lhs_logprobs) - (rhs_logprobs - lhs_logprobs) - 1
         kl_penalty = coeff * masked_mean(per_token_kl, padding_masks)
 
-        logger.collect("kl_penalty", kl_penalty)
-        logger.collect("kl_coeff", coeff)
+        logger.collect("kl_penalty.value", kl_penalty)
+        logger.collect("kl_penalty.coeff", coeff)
 
         return kl_penalty

ppotune/reward.py
@@ -150,7 +150,7 @@ class PerTokenKLPenalizedRewardModel(LLMRewardModel):
         pos_after_eos = get_unmasked_sequence_lengths(mask_after_eos)
 
         kl_coeff = float(self._kl_coeff)
-        rewards, kl, kl_rewards = get_rewards_ppo(
+        rewards, _, kl_rewards = get_rewards_ppo(
             scores,
             gen_logprobs,
             ref_logprobs,
@@ -158,8 +158,9 @@ class PerTokenKLPenalizedRewardModel(LLMRewardModel):
             pos_after_eos
         )
         logger.collect_dict({
-            "rlhf_reward": scores + kl_rewards.sum(1),
-            "kl_reward": kl_rewards.sum(1),
+            "reward.kl_coeff": torch.tensor(kl_coeff),
+            "reward.total": scores + kl_rewards.sum(1),
+            "reward.kl_penalty": kl_rewards.sum(1),
         })
         return rewards
 